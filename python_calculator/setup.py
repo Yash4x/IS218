404de6f8c2b4e032597a7215d2d84fb783917426
Yash Mandal <imyashmandal@gmail.com>
2025-09-19 15:33:02 -0400

    new stuff

---
+++ python_calculator/setup.py
@@ -0,0 +1,9 @@
+# IS218/python_calculator/setup.py
+from setuptools import setup
+
+setup(
+    name="calculator",
+    version="0.1.0",
+    py_modules=["calculator"],  # matches calculator.py
+    python_requires=">=3.8",
+)
